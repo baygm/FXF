9007e26a876076c08906f8cd6a3b127d5781439c
Kinder Boy <71623427+baygm@users.noreply.github.com>
2020-10-10 21:25:31 +0700

    -

--- app/src/main/java/com/bay/lib/Decryptor.java
+++ app/src/main/java/com/bay/lib/Decryptor.java
@@ -58,5 +58,20 @@ public class Decryptor
         } 
         return ascii; 
     }
-	
+	public static String encrypt(String strToEncrypt, String secret) 
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } 
+        catch (Exception e) 
+        {
+
+        }
+        return null;
+    }
+
 }
